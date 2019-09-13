5e97906ddc6bb2f7d89229d819728fcd3150a830
Bryant Richards <dragonseyeimages@gmail.com>
2019-09-13 22:49:04 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,7 +2,7 @@ katz_deli = []
 
 def line(array)
   place_array =[]
-  if array == 0
+  if array.length == 0
     puts "The line is currently empty."
   else
     array.each.with_index(1) do |name, index|
