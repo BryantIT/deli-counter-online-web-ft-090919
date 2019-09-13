adc3ee679aa5c1f65274ee4c8e7caf987d40dfe2
Bryant Richards <dragonseyeimages@gmail.com>
2019-09-13 22:43:18 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -8,5 +8,5 @@ def line(array)
     array.each.with_index(1) do |name, index|
       place_array << ("#{index}. #{name}")
     end
-    puts "The line is currently {place_array.join"
+    puts "The line is currently #{place_array.join(" ")"
 end
