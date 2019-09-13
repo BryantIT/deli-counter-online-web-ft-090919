72b6b5ab46110d4a80d0daafb854ab0bba5bd6a8
Bryant Richards <dragonseyeimages@gmail.com>
2019-09-13 22:56:46 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -14,6 +14,5 @@ def line(array)
   
   def take_a_number(katz_deli, name)
     katz_deli<< name
-    puts "Welcome, #{name}. You are "
-    
+    puts "Welcome, #{name}. You are #{katz_deli.length} in line"
   end
